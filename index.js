a34d071aec02670d587c406f06e3b4759acebf4d
kirti <kirtirauniyar097@gmail.com>
2024-04-19 00:33:29 +0530

    file crud

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const http = require('http');
-const data = require('./data');
-http.createServer((req,resp)=>{
+const fs = require('fs');
+const path = require('path');
+const dirpath = path.join(__dirname,'crud');
+const filepath = `${dirpath}/appple.text`;
 
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify(data));
-    resp.end();
+// fs.writeFileSync(filepath,'this is creating file with the help of writefilesynce mthod');//create file 
 
+// fs.readFile(filepath,'utf-8',(err,item)=>{
+//     console.log(item);
+// }) // reade file
 
-}).listen(5000);
+// fs.appendFile(filepath,'and file name is apple.text',(err)=>{
+//     if(!err) console.log("file text updated succesfully");
+// }) //update file
+
+// fs.rename(filepath,`${dirpath}/orrange.text`,(err)=>{
+//     if(!err) console.log("file name updated succesfully");
+// })
+
+fs.unlinkSync(`${dirpath}/text.txt`)
+
