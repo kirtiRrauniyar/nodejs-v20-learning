f914284f0639520f42f606f0241de07a449fe13a
kirti <kirtirauniyar097@gmail.com>
2024-04-18 23:38:00 +0530

    Initial-Commit

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:'kirti',email:'kirt@gmail.com',contact:'444444444'},
+    {name:'kk',email:'kk@gmail.com',contact:'5555555555'},
+    {name:'mm',email:'mm@gmail.com',contact:'6666666666'}
+]
+module.exports=data;
