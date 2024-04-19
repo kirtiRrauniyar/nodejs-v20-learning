d247c30f9cf29b2e0c5faf5681a1ac7b8633a697
kirti <kirtirauniyar097@gmail.com>
2024-04-19 23:55:28 +0530

    remone extention and open file with get method

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const path = require('path');
 const app = express();
 
 const publicpath = path.join(__dirname,'public');
- app.use(express.static(publicpath));
+//  app.use(express.static(publicpath));
+
+//remove route extention
+app.get('',(_,resp)=>{
+   resp.sendFile(`${publicpath}/index.html`);
+})
+
+app.get('/about',(_,resp)=>{
+    resp.sendFile(`${publicpath}/about.html`);
+ })
+
+ app.get('/help',(_,resp)=>{
+    resp.sendFile(`${publicpath}/help.html`);
+ })
+
+ app.get('*',(_,resp)=>{
+    resp.sendFile(`${publicpath}/404.html`);
+ })
 
 app.listen(5000);
