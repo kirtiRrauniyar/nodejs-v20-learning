9847c202b9eb8834eb2f0508ed4a899d312cb58f
kirti <kirtirauniyar097@gmail.com>
2024-04-19 23:31:57 +0530

    link html render req data in html page render

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 
 app.get('',(req,res)=>{
-    console.log("data requested by browser",req.query.name);
-    res.send("hi home page" + req.query.name);
+    // console.log("data requested by browser",req.query.name);
+    res.send(`
+      <h1>hi home page  ${req.query.name}</h1>
+       <a href="/about">Go to about page</a>
+      <a href="/contact">Go to contact page</a>
+    `);
 })
 
 app.get('/about',(req,res)=>{
-    res.send("hi about page");
+    res.send(`
+    <h1>hi about page</h1><a href="/">Go to home page</a>
+    `);
+})
+
+app.get('/contact',(req,res)=>{
+    res.send(`
+    <input placeholder="use name" value="${req.query.name}" />
+    <button>back</button>
+    <a href="/">Go to home page</a>
+    `);
 })
 
 app.listen(5000);
