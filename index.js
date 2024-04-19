1334e0cec40541a7add693e28ceb2aa569063aa6
kirti <kirtirauniyar097@gmail.com>
2024-04-19 23:44:44 +0530

    link file.html public from folder with path fun

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('',(req,res)=>{
-    // console.log("data requested by browser",req.query.name);
-    res.send(`
-      <h1>hi home page  ${req.query.name}</h1>
-       <a href="/about">Go to about page</a>
-      <a href="/contact">Go to contact page</a>
-    `);
-})
-
-app.get('/about',(req,res)=>{
-    res.send(`
-    <h1>hi about page</h1><a href="/">Go to home page</a>
-    `);
-})
-
-app.get('/contact',(req,res)=>{
-    res.send(`
-    <input placeholder="use name" value="${req.query.name}" />
-    <button>back</button>
-    <a href="/">Go to home page</a>
-    `);
-})
+const publicpath = path.join(__dirname,'public');
+ app.use(express.static(publicpath));
 
 app.listen(5000);
